4b4669d1458e00908f186d89e821dd2cfa2c3586
Riff-Tone <dabohe27@gmail.com>
2024-10-04 22:14:45 -0700

    make some noise for addiction!!!!

---
+++ library.h
@@ -0,0 +1,17 @@
+//
+//  library.h
+//  A6
+//
+//  Created by Diego Gayosso Hernandez on 10/4/24.
+//
+
+#ifndef library_h
+#define library_h
+
+#include <stdio.h>
+
+void readFile(int size);
+void writeFile(void);
+void clearInputBuffer(void);
+char toQuit(void);
+#endif /* library_h */
