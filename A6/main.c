3e4377cbd7d2fe2e01dfad3451e2ab0eb58c8d03
Riff-Tone <dabohe27@gmail.com>
2024-10-04 20:31:57 -0700

    Initial Commit

---
+++ A6/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  A6
+//
+//  Created by Diego Gayosso Hernandez on 10/4/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
